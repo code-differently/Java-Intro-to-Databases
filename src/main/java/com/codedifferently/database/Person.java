d003423ecbdb2f022210c9a99b6f6de2e9de6674
Matt Werth <matthewwerth@gmail.com>
2021-01-05 18:36:09 -0500

    Initial Commit W/ Person Class

---
+++ src/main/java/com/codedifferently/database/Person.java
@@ -0,0 +1,47 @@
+package com.codedifferently.database;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String email;
+
+    public Person(String firstName, String lastName, Integer age, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
