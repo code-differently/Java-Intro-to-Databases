e4c7230e033ca3a5ae4d2de850c5a211e33cd45d
Matt Werth <matthewwerth@gmail.com>
2021-01-05 18:54:40 -0500

    Added AddressBook

---
+++ src/main/java/com/codedifferently/database/AddressBook.java
@@ -0,0 +1,47 @@
+package com.codedifferently.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private Person owner;
+    private List<Person> people;
+
+    public AddressBook(Person owner) {
+        this.owner = owner;
+        people = new ArrayList<>();
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public void addPerson(Person person) {
+        this.people.add(person);
+    }
+
+    public void removePerson(Person person) {
+        this.people.remove(person);
+    }
+
+    public Person getPersonByEmail(String email) {
+        return people.stream()
+                .filter(person -> person.getEmail().equals(email))
+                .findFirst()
+                .orElseThrow(NullPointerException::new);
+    }
+
+    public List<Person> getAllPeople() {
+        return people;
+    }
+
+    public boolean saveAll() {
+        //if() return true;
+        return false;
+    }
+
+}
