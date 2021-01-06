bd7140760a7a5354e66a0facada4296a5f20fd8a
Matt Werth <matthewwerth@gmail.com>
2021-01-05 19:28:03 -0500

    Added Unit Tests For Person

--- src/test/java/com/codedifferently/database/AddressBookTest.java
+++ src/test/java/com/codedifferently/database/AddressBookTest.java
@@ -4,13 +4,20 @@ import junit.framework.TestCase;
 
 public class AddressBookTest extends TestCase {
     AddressBook addressBook;
+    DataBase dataBase;
+    Person per1;
+    Person per2;
 
     public void setUp() throws Exception {
-        //addressBook = new AddressBook();
+        dataBase = new DataBase();
+        addressBook = new AddressBook(dataBase);
+        per1 = new Person("Matt", "Werth", 33, "matthewwerth@gmail.com");
+        per2 = new Person("Laurie", "Werth", 33, "lauriewerth@gmail.com");
     }
 
     public void testGetOwner() {
-
+        addressBook.setOwner(per1);
+        System.out.println(addressBook.getOwner());
     }
 
     public void testSetOwner() {
---
+++ src/test/java/com/codedifferently/database/PersonTest.java
@@ -0,0 +1,92 @@
+package com.codedifferently.database;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    Person person;
+
+    @Before
+    public void setUp() throws Exception {
+         person = new Person("Matt", "Werth", 33, "matthewwerth@gmail.com");
+    }
+
+    @Test
+    public void getFirstName() {
+        String actual = person.getFirstName();
+        String expected = "Matt";
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setFirstName() {
+        person.setFirstName("Marty");
+        String actual = person.getFirstName();
+        String expected = "Marty";
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getLastName() {
+        String actual = person.getLastName();
+        String expected = "Werth";
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setLastName() {
+        person.setLastName("Wyler");
+        String actual = person.getLastName();
+        String expected = "Wyler";
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAge() {
+        Integer actual = person.getAge();
+        Integer expected = 33;
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setAge() {
+        person.setAge(31);
+        Integer actual = person.getAge();
+        Integer expected = 31;
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getEmail() {
+        String actual = person.getEmail();
+        String expected = "matthewwerth@gmail.com";
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setEmail() {
+        person.setEmail("testThis@gmail.com");
+        String actual = person.getEmail();
+        String expected = "testThis@gmail.com";
+        System.out.println(actual);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
